8d0dbf36469d5b53a1a5536366a743662a917b13
fortunamoon <casey.chervenka@outlook.com>
2020-12-28 15:23:15 -0600

    Updated for Compamrisons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Card:
@@ -24,36 +25,87 @@ class Card:
         return self.name
 
 
-def run_scenario(card, paymemt_percent):
-    month = 0
-    total_interest = 0
-    df_data = pd.DataFrame(columns=('Month', 'Interest', 'Balance', "Total_Interest"))
-
-    while card.balance >0:
-        card.balance = card.balance - (card_balance * (paymemt_percent / 100))
-        new_interest = card.monthly_interest()
-        total_interest += new_interest
-        new_balance = card.rollover_balance()
-        df_data.loc[month] = [month] + [new_interest] + [new_balance] + [total_interest]
-        month+=1
+def plot_data(df, note):
     ax = plt.gca()
     # ax.set_title(f'{card.name} paid in {month} months')
-    df_data.plot(kind='line', x='Month', y='Total_Interest', ax=ax)
-    df_data.plot(kind='line', x='Month', y='Balance', color='red', ax=ax)
-    ax.text(4, 40, f'It took {month} months to pay off your {card.name} and you paid ${round(total_interest,2)} in interest', style='italic',
+    df.plot(kind='line', x='Month', y='Total_Interest', ax=ax)
+    df.plot(kind='line', x='Month', y='Balance', color='red', ax=ax)
+    ax.text(4, 40,
+            f'{note}',
+            style='italic',
             bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
-
     plt.show()
 
 
+def plot_data_summary(df, note):
+    df2 = df.pivot(index='Month', columns='Card', values='Total_Interest')
+    df = df.pivot(index='Month', columns='Card', values='Balance')
+    ax = df2.plot()
+    df.plot(kind='bar', ax=ax)
+    ax.set_title('Card Comparison')
+    ax.set_ylabel('Total Interest in $')
+    ax.set_xlabel('Months')
+    ax.xaxis.set_major_locator(plt.MultipleLocator(10))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(5))
+    plt.savefig('Summary_Graph.svg')
+    plt.show()
+
+
+def run_scenario(card, paymemt_percent, min_payment):
+    month = 0
+    total_interest = 0
+    df_data = pd.DataFrame(columns=('Card', 'Month', 'Interest', 'Balance', "Total_Interest"))
+
+    while card.balance > 0:
+        if card.balance * (paymemt_percent / 100) < min_payment:
+            card.balance = card.balance - min_payment
+        else:
+            card.balance = card.balance - (card.balance * (paymemt_percent / 100))
+        new_interest = card.monthly_interest()
+        total_interest += new_interest
+        new_balance = card.rollover_balance()
+        df_data.loc[month] = [card.name] + [month] + [new_interest] + [new_balance] + [total_interest]
+        month+=1
+    note = f'It took {month} months to pay off your {card.name} and you paid ${round(total_interest, 2)} in interest'
+    plot_data(df_data, note)
+
+    return df_data
+
+
+def collector():
+    try:
+        card_name = input('Welcome, please enter the name of your credit card:')
+        card_interest = float(input(f'Please enter the {card_name}s interest rate:'))
+        card_balance = float(input(f'Please enter the current balance for the {card_name}'))
+        payment_percentage = float(input('Please enter the percentage of the balance you wish to pay every month or press enter for 2%:') or 2)
+        minimum_payment = float(input(f'Please enter the minimum payment you wish to pay every month if {payment_percentage}% is to small') or 25)
+        payment_amount = card_balance * (payment_percentage / 100)
+        loaded_card = Card(card_name, card_interest, card_balance)
+        df_scenario_data = run_scenario(loaded_card, payment_percentage, minimum_payment)
+        run_another = input('Would you like to run another scenario? Y/N')
+
+        return 1, df_scenario_data, run_another
+    except ValueError as e:
+        return e
+
+
 if __name__ == '__main__':
-    card_name = input('Welcome, please enter the name of your credit card:')
-    card_interest = float(input(f'Please enter the {card_name}s interest rate:'))
-    card_balance = float(input(f'Please enter the current balance for the {card_name}'))
-    payment_percentage = float(input('Please enter the percentage of the balance you wish to pay every month or press enter for 2%:') or 2)
-    payment_amount = card_balance * (payment_percentage / 100)
-    loaded_card = Card(card_name, card_interest, card_balance)
-    run_scenario(loaded_card, payment_percentage)
+    success = collector()
+    df_summary = pd.DataFrame(columns=('Card', 'Month', 'Interest', 'Balance', "Total_Interest"))
+    if success[0] == 1:
+
+        while success[2].lower() =='y':
+            df_summary = df_summary.append(success[1])
+            success = collector()
+        df_summary = df_summary.append(success[1])
+        to_compare = input('Would you like to compare the cards? Y/N:')
+        if to_compare.lower() == 'y':
+            plot_data_summary(df_summary, 'Summary')
+    else:
+        print('\n*******************************')
+        print(f'\nThere is a problem, {success}')
+
+
 
 
 
